899b8e33dccd63e2f2e7daf4dc2b641220fa9729
ReekyStive <reekystive@gmail.com>
2023-02-22 18:40:49 +0800

    Add vue-router

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify.ts'
+import router from './plugins/router.ts'
 
 
 createApp(App)
   .use(vuetify)
+  .use(router)
   .mount('#app')
---
+++ frontend/src/plugins/router.ts
@@ -0,0 +1,13 @@
+import * as VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: () => import('../views/About.vue') },
+]
+
+export default VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes,
+})
